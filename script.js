432271cfef5a7a382f1c82e1db47fc9ca373e01b
Luana Ernst <luanaernst33@gmail.com>
2026-06-11 16:06:03 +0200

    js verlinkt

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 fetch('https://api.github.com/repos/Luana3333333/BugCatalogue/contents/_kaefer')
   .then(response => response.json())
   .then(files => {
-    console.log(files)
+    files.forEach(file => {
+      fetch(file.download_url)
+        .then(response => response.text())
+        .then(content => {
+          console.log(content)
+        })
+    })
   })
