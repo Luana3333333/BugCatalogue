86e6180ea60d6ff6aed67b859704b6644fc01bdb
Luana Ernst <luanaernst33@gmail.com>
2026-06-11 17:54:26 +0200

    console log

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function parseFrontmatter(text) {
+  const parts = text.split('---')
+  const lines = parts[1].trim().split('\n')
+  const data = {}
+  
+  lines.forEach(line => {
+    const parts = line.split(':')
+    const key = parts[0].trim()
+    const value = parts[1].trim()
+    data[key] = value
+  })
+  
+  return data
+}
+
 fetch('https://api.github.com/repos/Luana3333333/BugCatalogue/contents/_kaefer')
   .then(response => response.json())
   .then(files => {
@@ -5,7 +20,8 @@ fetch('https://api.github.com/repos/Luana3333333/BugCatalogue/contents/_kaefer')
       fetch(file.download_url)
         .then(response => response.text())
         .then(content => {
-          console.log(content)
+          const data = parseFrontmatter(content)
+          console.log(data)
         })
     })
   })
